154e68e6226b6c79c9ffe55f9af16550b2047feb
Arvin Buid <arvinbuid01@gmail.com>
2025-10-27 18:59:46 +0800

    feat: implement add new product

---
+++ app/add-product/page.tsx
@@ -0,0 +1,125 @@
+import { createProduct } from "@/lib/actions/products";
+import Sidebar from "../components/sidebar";
+import Link from "next/link";
+
+const AddProductPage = async () => {
+
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Sidebar currentPath="/add-product" />
+
+            <main className="ml-64 p-6">
+                <div className="mb-8">
+                    <div className="flex items-center justify-between mb-6">
+                        <div>
+                            <h1 className="text-2xl font-semibold text-gray-900">Add Product</h1>
+                            <p className="text-sm text-gray-500 mt-1">Add a new product into your inventory.</p>
+                        </div>
+                    </div>
+                    <div className="max-w-xl">
+                        <div className="bg-white p-6 border border-gray-200 rounded-lg shadow">
+                            <form className="space-y-6" action={createProduct}>
+                                {/* Product Name */}
+                                <div>
+                                    <label
+                                        htmlFor="name"
+                                        className="block text-sm font-medium text-gray-700 mb-2"
+                                    >
+                                        Product Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="name"
+                                        placeholder="Add a new product"
+                                        className="text-sm px-4 py-2 text-gray-700 placeholder-gray-400 w-full border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
+                                        required
+                                    />
+                                </div>
+                                {/* Price and Quantity */}
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                    <div>
+                                        <label
+                                            htmlFor="price"
+                                            className="block text-sm font-medium text-gray-700 mb-2"
+                                        >
+                                            Price
+                                        </label>
+                                        <input
+                                            type="number"
+                                            id="price"
+                                            name="price"
+                                            className="text-sm px-4 py-2 text-gray-700 w-full border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <label
+                                            htmlFor="quantity"
+                                            className="block text-sm font-medium text-gray-700 mb-2"
+                                        >
+                                            Quantity
+                                        </label>
+                                        <input
+                                            type="number"
+                                            id="quantity"
+                                            name="quantity"
+                                            className="text-sm px-4 py-2 text-gray-700 w-full border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                {/* Sku */}
+                                <div>
+                                    <label
+                                        htmlFor="sku"
+                                        className="block text-sm font-medium text-gray-700 mb-2"
+                                    >
+                                        SKU (Optional)
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="sku"
+                                        name="sku"
+                                        placeholder="Enter SKU"
+                                        className="text-sm px-4 py-2 text-gray-700 placeholder-gray-400 w-full border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
+                                    />
+                                </div>
+                                {/* Low Stock Threshold*/}
+                                <div>
+                                    <label
+                                        htmlFor="lowstockAt"
+                                        className="block text-sm font-medium text-gray-700 mb-2"
+                                    >
+                                        Low Stock At (Optional)
+                                    </label>
+                                    <input
+                                        type="number"
+                                        id="lowstockAt"
+                                        name="lowstockAt"
+                                        placeholder="Enter low stock threshold"
+                                        className="text-sm px-4 py-2 text-gray-700 placeholder-gray-400 w-full border border-gray-300 rounded-md focus:outline-none focus:border-gray-400"
+                                    />
+                                </div>
+                                {/* Submit and Cancel Buttons */}
+                                <div className="flex items-center gap-3 mt-4">
+                                    <button type="submit" className="px-4 py-3 text-white rounded-md bg-violet-500 text-sm cursor-pointer">
+                                        Add Product
+                                    </button>
+                                    <Link
+                                        href='/add-product'
+                                        className="px-4 py-3 text-gray-900 rounded-md bg-gray-400/30 text-sm cursor-pointer"
+                                    >
+                                        Cancel
+                                    </Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default AddProductPage;
--- lib/actions/products.ts
+++ lib/actions/products.ts
@@ -1,7 +1,42 @@
 "use server";
 
+import {redirect} from "next/navigation";
 import {getCurrentUser} from "../auth";
 import {prisma} from "../client";
+import z from "zod";
+
+const ProductSchema = z.object({
+  name: z.string().min(3, "Product name must be at least 3 characters long"),
+  price: z.coerce.number().nonnegative("Price must be a positive number"),
+  quantity: z.coerce.number().int().min(0, "Quantity must be a positive number"),
+  sku: z.string().optional(),
+  lowStockAt: z.coerce.number().int().min(0).optional(),
+});
+
+export const createProduct = async (formData: FormData) => {
+  const user = await getCurrentUser();
+  const userId = user.id;
+
+  const parsed = ProductSchema.safeParse({
+    name: formData.get("name"),
+    price: formData.get("price"),
+    quantity: formData.get("quantity"),
+    sku: formData.get("sku") || undefined,
+    lowStockAt: formData.get("lowStockAt") || undefined,
+  });
+
+  if (!parsed.success) throw new Error("Validation failed!");
+
+  try {
+    await prisma.product.create({
+      data: {...parsed.data, userId},
+    });
+  } catch (e) {
+    console.error("Prisma error: ", e);
+    throw new Error("Failed to create product");
+  }
+  redirect("/inventory");
+};
 
 export const deleteProduct = async (formData: FormData) => {
   const user = await getCurrentUser();
