e524c93b51a8b21ff8587a28bcaaf155101f8017
Arvin Buid <arvinbuid01@gmail.com>
2025-10-30 10:55:47 +0800

    feat: build landing page and add two tailwind utility classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,44 @@
+import Image from "next/image";
+
+import Picture from '../public/images/landing-image.svg';
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="space-y-5 max-w-[700px] text-center">
-        <h1 className="text-4xl mb-6">Inventory Management System</h1>
-        <p>
-          Tired of stockouts, surplus inventory, and confusing spreadsheets? It&apos;s time to take complete control. Our Inventory Management System is the powerful, intuitive solution designed to revolutionize how you track, manage, and optimize your entire stock lifecycle.
-        </p>
-        <div className="flex gap-4 mt-10 justify-center">
+    <div className="h-screen bg-gray-50 relative flex items-center">
+      {/* Top Rectangle */}
+      <div className="h-24 md:h-36 bg-[#6C63FF] w-full rectangle-top absolute top-0"></div>
 
-          <button className="px-10 py-2 bg-blue-600 rounded-lg text-white cursor-pointer text-md">
-            <Link href={'/sign-in'}>Sign In</Link></button>
-          <button className="px-6 py-4 border-3 text-blue-700 border-blue-600 cursor-pointer rounded-xl">Learn More</button>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+        {/* Left Section */}
+        <div className="flex flex-col justify-center items-center">
+          <div className=" max-w-md pl-12 pr-6 space-y-5">
+            <h1 className="text-violet-800 text-4xl md:text-5xl font-mono pl-2 font-semibold">Omnistock</h1>
+            <p className="text-lg: md:text-xl">A powerful modern solution designed to improve how you track, manage, and optimize your entire stock lifecycle.</p>
+            <Link href='/dashboard'>
+              <button
+                className="px-10 py-3 text-white bg-[#6C63FF] rounded-lg uppercase font-mono cursor-pointer text-sm md:text-md lg:text-lg hover:bg-[#524dc2] transition-colors ease-in"
+              >
+                Get Started
+              </button>
+            </Link>
+          </div>
+        </div>
+        {/* Right Section */}
+        <div className="hidden md:flex justify-center">
+          <Image
+            src={Picture}
+            alt="picture"
+            width={0}
+            height={0}
+            className="w-[550px] h-auto"
+
+          />
         </div>
       </div>
+
+      {/* Bottom Rectangle */}
+      <div className="h-24 md:h-36 bg-[#6C63FF] w-full rectangle-bottom absolute bottom-0"></div>
     </div>
   );
 }
