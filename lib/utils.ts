4ff281e6f267e8f6894e4556d57ad2d5f75cb9e6
Arvin Buid <arvinbuid01@gmail.com>
2025-11-02 12:17:58 +0800

    feat: create formatCurrency utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,3 +2,20 @@
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-PH", {
+  currency: "PHP",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+
+// Format currency using the formatter above
+export const formatCurrency = (amount: number | string | null) => {
+  if (typeof amount === "number") {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === "string") {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return "Not a number!";
+  }
+};
