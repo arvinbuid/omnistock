ca2347ec377b0bbbd7dd606f54a7436ca445ad7d
Arvin Buid <arvinbuid01@gmail.com>
2025-11-03 10:18:15 +0800

    feat: implement sidebar toggle close/open functionality

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -3,46 +3,55 @@
 import { UserButton } from "@stackframe/stack";
 import { BarChart3, Package, Plus, Settings } from "lucide-react";
 import Link from "next/link";
+import { useSidebar } from "../context/SidebarContext";
 
 interface SidebarProps {
     currentPath: string;
 }
 
+const navLinks = [
+    {
+        name: 'Dashboard',
+        href: '/dashboard',
+        icon: BarChart3
+    },
+    {
+        name: 'Inventory',
+        href: '/inventory',
+        icon: Package
+    },
+    {
+        name: 'Add New Product',
+        href: '/add-product',
+        icon: Plus
+    },
+    {
+        name: 'Settings',
+        href: '/settings',
+        icon: Settings
+    },
+]
+
 const Sidebar = ({ currentPath = '/dashboard' }: SidebarProps) => {
-    const navLinks = [
-        {
-            name: 'Dashboard',
-            href: '/dashboard',
-            icon: BarChart3
-        },
-        {
-            name: 'Inventory',
-            href: '/inventory',
-            icon: Package
-        },
-        {
-            name: 'Add New Product',
-            href: '/add-product',
-            icon: Plus
-        },
-        {
-            name: 'Settings',
-            href: '/settings',
-            icon: Settings
-        },
-    ]
+    const { isOpen, toggleSidebar } = useSidebar();
+
     return (
-        <div className="fixed left-0 top-0 min-h-screen text-white w-64 bg-gray-800 p-6 z-10">
+        <div className={`fixed top-0 min-h-screen text-white bg-gray-800 p-6 z-10 transition-all duration-300 ${isOpen ? 'w-64 left-0' : 'w-64 -left-49'}`}>
             {/* Sidebar Logo */}
             <div className="mb-8 mt-3">
                 <div className="flex items-center gap-2 px-2 relative">
-                    <BarChart3 className="w-6 h-6" />
-                    <h1>OmniStock</h1>
-                    <span className="rotate-45 text-2xl absolute -top-3.5 right-0 cursor-pointer">+</span>
+                    <BarChart3 className={`w-6 h-6 transition-opacity duration-100 ${isOpen ? 'opacity-100' : 'opacity-0'}`} />
+                    <h1 className={`transition-opacity duration-100 ${isOpen ? 'opacity-100' : 'opacity-0'}`}>OmniStock</h1>
+                    <span
+                        onClick={toggleSidebar}
+                        className="rotate-45 text-2xl absolute -top-3.5 right-0 cursor-pointer"
+                    >
+                        +
+                    </span>
                 </div>
             </div>
             {/* Nav Links */}
-            <nav className="space-y-1 px-2">
+            <nav className={`space-y-1 px-2  transition-opacity duration-100 ${isOpen ? 'opacity-100' : 'opacity-0'}`}>
                 <div className="text-xs uppercase text-gray-400 font-semibold">Inventory</div>
                 <ul className="text-sm space-y-1 mt-5">
                     {navLinks.map((link, index) => {
@@ -61,7 +70,7 @@ const Sidebar = ({ currentPath = '/dashboard' }: SidebarProps) => {
                 </ul>
             </nav>
             {/* Sidebar Footer */}
-            <div className="absolute bottom-0 left-0 right-0 border-t border-gray-700 p-4 w-[calc(100%-32px)] mx-auto">
+            <div className={`absolute bottom-0 left-0 right-0 border-t border-gray-700 p-4 w-[calc(100%-32px)] mx-auto transition-opacity duration-100 ${isOpen ? 'opacity-100' : 'opacity-0'}`}>
                 <div className="flex items-center justify-between text-sm ">
                     <UserButton showUserInfo />
                 </div>
