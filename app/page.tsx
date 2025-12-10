6ee8ba42482ddc109dad2899f9d22adfad3b0998
Arvin Buid <arvinbuid01@gmail.com>
2025-12-10 14:58:24 +0800

    style: add responsive media query in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -16,29 +16,31 @@ const roobertRegular = localFont({
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-gray-50 relative flex items-center justify-center">
-      <div className="w-[65%] max-w-[65%] h-[85%] bg-[#4642f0] rounded-2xl flex">
-        <div className="max-w-xl h-full p-16">
-          <div className="mb-4">
-            <h1 className={`text-6xl text-[#FBD0FF] font-bold leading-snug tracking-tight ${roobertBold.className}`}>Smarter inventory for business & teams</h1>
+      <div className="w-[65%] max-w-[65%] h-[85%] bg-[#4642f0] rounded-2xl flex justify-between">
+        <div className="max-w-xl p-10 md:p-16">
+          <div className="flex-1">
+            <div className="mb-4">
+              <h1 className={`text-4xl md:text-6xl text-[#FBD0FF] font-bold leading-snug tracking-tight ${roobertBold.className}`}>Smarter inventory for business & teams</h1>
+            </div>
+            <p className={`pl-1 text-[#FBD0FF] ${roobertRegular.className} mb-5`}>
+              Focused on simplifying and empowering efficient, real-time inventory control.
+            </p>
+            <Link href='/dashboard'>
+              <button className={`px-4 py-3 text-sm text-black ${roobertBold.className} bg-[#FBD0FF] hover:bg-[#eba6f1] transition-colors rounded-xl cursor-pointer flex items-center gap-2 rounded-l-[18px] rounded-r-[20px]`}>
+                Start Now
+                <span><ChevronRight /></span>
+              </button>
+            </Link>
           </div>
-          <p className={`pl-1 text-[#FBD0FF] ${roobertRegular.className} mb-5`}>
-            Focused on simplifying and empowering efficient, real-time inventory control.
-          </p>
-          <Link href='/dashboard'>
-            <button className={`px-4 py-3 text-sm text-black ${roobertBold.className} bg-[#FBD0FF] hover:bg-[#eba6f1] transition-colors rounded-xl cursor-pointer flex items-center gap-2 rounded-l-[18px] rounded-r-[20px]`}>
-              Start Now
-              <span><ChevronRight /></span>
-            </button>
-          </Link>
         </div>
         {/* Image */}
-
-        <Image
-          src={Picture}
-          alt='Business Woman'
-          className="rounded-r-2xl w-full object-cover"
-        />
-
+        <div>
+          <Image
+            src={Picture}
+            alt='Business Woman'
+            className="rounded-r-2xl w-full h-full object-cover hidden lg:block"
+          />
+        </div>
       </div>
     </div>
   );
