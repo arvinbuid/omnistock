158432323dcb83f84122e9ff3c87e37221228e23
Arvin Buid <arvinbuid01@gmail.com>
2025-12-10 17:02:59 +0800

    fix: update responsive media query and button link in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const roobertRegular = localFont({
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-gray-50 relative flex items-center justify-center">
-      <div className="w-[65%] max-w-[65%] h-[85%] bg-[#4642f0] rounded-2xl flex justify-between">
+      <div className="w-full md:w-[65%] max-w-full mx-14 md:mx-0 md:max-w-[65%] h-[85%] bg-[#4642f0] rounded-2xl flex justify-between">
         <div className="max-w-xl p-10 md:p-16">
           <div className="flex-1">
             <div className="mb-4">
@@ -25,7 +25,7 @@ export default function Home() {
             <p className={`pl-1 text-[#FBD0FF] ${roobertRegular.className} mb-5`}>
               Focused on simplifying and empowering efficient, real-time inventory control.
             </p>
-            <Link href='/dashboard'>
+            <Link href='/sign-in'>
               <button className={`px-4 py-3 text-sm text-black ${roobertBold.className} bg-[#FBD0FF] hover:bg-[#eba6f1] transition-colors rounded-xl cursor-pointer flex items-center gap-2 rounded-l-[18px] rounded-r-[20px]`}>
                 Start Now
                 <span><ChevronRight /></span>
